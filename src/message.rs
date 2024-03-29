a5ea396d60169f0e7f591bbd721b53604c847cb9
Sung <hello@sungwoncho.com>
2024-03-29 21:00:46 +1100

    Parse update message

--- src/message.rs
+++ src/message.rs
@@ -1,5 +1,6 @@
 use std::io::Read;
 use std::io;
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 // maximum message size is 4096 octets.
 const BGP_HEADER_SIZE_BYTES: usize = 19;
@@ -11,20 +12,110 @@ enum MessageType {
     KEEPALIVE = 4,
 }
 
+impl MessageType {
+    fn from_u8(value: u8) -> Option<MessageType> {
+        match value {
+            1 => Some(MessageType::OPEN),
+            2 => Some(MessageType::OPEN),
+            3 => Some(MessageType::OPEN),
+            4 => Some(MessageType::OPEN),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum IpNetwork {
+    V4(Ipv4Net),
+    V6(Ipv6Net),
+}
+
+#[derive(Debug, PartialEq)]
+struct Ipv4Net {
+    address: Ipv4Addr,
+    prefix_length: u8,
+}
+
+#[derive(Debug, PartialEq)]
+struct Ipv6Net {
+    address: Ipv6Addr,
+    prefix_length: u8,
+}
+
 pub enum BgpMessage {
     Open(OpenMessage),
+    Update(UpdateMessage),
 }
 
 impl BgpMessage {
     fn from_bytes(message_type: u8, bytes: Vec<u8>) -> Result<Self, io::Error> {
-        if message_type == MessageType::OPEN as u8 {
-            let open_message = OpenMessage::from_bytes(bytes)?;
-            Ok(BgpMessage::Open(open_message))
-        } else {
-            Err(io::Error::new(io::ErrorKind::Other, "Unknown message type"))
+        match MessageType::from_u8(message_type) {
+            Some(MessageType::OPEN) => {
+                let open_message = OpenMessage::from_bytes(bytes)?;
+                Ok(BgpMessage::Open(open_message))
+            },
+            Some(MessageType::UPDATE) => {
+                let update_message = UpdateMessage::from_bytes(bytes)?;
+                Ok(BgpMessage::Update(update_message))
+            }
+            _ => {
+                Err(io::Error::new(io::ErrorKind::Other, "Unknown message type"))
+            }
         }
     }
 }
+
+struct UpdateMessage {
+    withdrawn_routes_length: u16,
+    withdrawn_routes: Vec<IpNetwork>,
+    total_path_attribute_length: u16,
+    // path_attributes: ?
+}
+
+fn parse_nlri_list(bytes: Vec<u8>) -> Vec<IpNetwork> {
+    let mut cursor = 0;
+
+    let mut nlri_list: Vec<IpNetwork> = Vec::new();
+
+    while cursor < bytes.len() {
+        let prefix_length = bytes[cursor];
+        cursor = cursor + 1;
+
+        let byte_len: usize = (prefix_length as usize + 7) / 8;
+
+        let mut buff = [0; 4];
+        for i in 0..byte_len {
+            buff[i] = bytes[cursor + i];
+        }
+
+        let address = Ipv4Addr::from(buff);
+        let network = IpNetwork::V4(Ipv4Net{address, prefix_length: prefix_length});
+
+        nlri_list.push(network);
+
+        cursor = cursor + byte_len + 1;
+    }
+
+    return nlri_list
+}
+
+impl UpdateMessage {
+    fn from_bytes(bytes: Vec<u8>) -> Result<Self, io::Error> {
+        let withdrawn_routes_length = u16::from_be_bytes([bytes[0], bytes[1]]);
+
+        let withdraw_routes_end = (2 + withdrawn_routes_length) as usize;
+        let withdrawn_routes = parse_nlri_list(bytes[2..withdraw_routes_end].to_vec());
+
+        let total_path_attribute_length = u16::from_be_bytes([bytes[withdraw_routes_end], bytes[withdraw_routes_end+2]]);
+
+        Ok(UpdateMessage{
+            withdrawn_routes_length,
+            withdrawn_routes,
+            total_path_attribute_length,
+        })
+    }
+}
+
 
 struct OpenMessage {
     version: u8,
@@ -46,7 +137,6 @@ impl OpenMessage {
         let hold_time = u16::from_be_bytes([bytes[3], bytes[4]]);
         let bgp_identifier = u32::from_be_bytes([bytes[5], bytes[6], bytes[7], bytes[8]]);
         let optional_parameters_length = bytes[9];
-        println!("{} {} {} {}", version, asn ,hold_time, bgp_identifier);
 
         Ok(OpenMessage{
             version,
@@ -91,9 +181,9 @@ mod tests {
 
     #[test]
     fn test_read_open_message() {
-        let mock_stream = Cursor::new(MOCK_OPEN_MESSAGE);
+        let stream = Cursor::new(MOCK_OPEN_MESSAGE);
 
-        match read_bgp_message(mock_stream) {
+        match read_bgp_message(stream) {
             Ok(BgpMessage::Open(open_message)) => {
                 assert_eq!(open_message.version, 4);
                 assert_eq!(open_message.asn, 1234);
@@ -101,7 +191,39 @@ mod tests {
                 assert_eq!(open_message.bgp_identifier, 168430090);
                 assert_eq!(open_message.optional_parameters_length, 0);
             }
-            Err(err) => panic!("Error {}", err)
+            Err(err) => panic!("Error {}", err),
+            _ => panic!("Expected BgpMessage::OPEN")
         }
+    }
+
+    #[test]
+    fn test_parse_nlri_list_single() {
+        let data: Vec<u8> = vec!(
+            0x18,
+            0x0a, 0x0b, 0x0c, 0x00,
+        );
+
+        let result = parse_nlri_list(data);
+        let expected = vec!(
+            IpNetwork::V4(Ipv4Net{address: Ipv4Addr::new(10 , 11, 12, 0), prefix_length: 24}),
+        );
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_nlri_list_multiple() {
+        let data: Vec<u8> = vec!(
+            0x18,
+            0x0a, 0x0b, 0x0c, 0x00,
+            0x15,
+            0x0a, 0x0b, 0x08, 0x00,
+        );
+
+        let result = parse_nlri_list(data);
+        let expected = vec!(
+            IpNetwork::V4(Ipv4Net{address: Ipv4Addr::new(10 , 11, 12, 0), prefix_length: 24}),
+            IpNetwork::V4(Ipv4Net{address: Ipv4Addr::new(10 , 11, 8, 0), prefix_length: 21}),
+        );
+        assert_eq!(result, expected);
     }
 }
